7af29dce2a246f43df32164cc60432a13a110a6c
meilech1 <c.meile@mail.utoronto.ca>
2023-06-10 09:27:54 -0400

    Found a better way to check if user input is a number. as.numeric() will return NA if the user input value cannot be converted to number.

--- Assignment2_Meile_Christopher.R
+++ Assignment2_Meile_Christopher.R
@@ -9,16 +9,32 @@
 
 user_value <- readline(prompt = "Please enter a three digit positive number: ")
 
-print(is.na(user_value))
+
+#* When a user input consists of non-digits (e.g. "abc", "%x1") the as.numeric()
+#* function returns NA because it cannot cast those strings to numbers. As such,
+#* the if loop below is a condition to check the user input for non-digits. If
+#* there are non digit characters, the if statement returns FALSE. If all the
+#* elements in user_value are digits, they can be cast to as.numeric(). As such,
+#* the IF statement returns TRUE. 
+
+if(!is.na(as.numeric(user_value))){
+  # TRUE if user_value has been successfully casted to as.numeric()
+  print("Number input")
+  
+} else {
+  # FALSE if user_value includes any non-number elements (e.g. "1hs")
+  print("Non-number input")
+}
+
 # Check if user_value is numeric when cast in as.numeric function. 
 # Assuming, number has to be > 0 as 0 is neither positive or negative (000)
 # Check if it's an integer
-if(is.numeric(as.numeric(user_value)) & as.numeric(user_value) > 0) {
+#if(is.numeric(as.numeric(user_value)) & as.numeric(user_value) > 0) {
   # is a number and greater than 0 
-  print("number greater than 0")
+  #print("number greater than 0")
   
-} else {
+#} else {
   # User input is not a positive number 
-  print("not a positive number")
-}
+ # print("not a positive number")
+#}
 
