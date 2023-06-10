d1d9bf6b48e2e194b143c5de1c3916b22e688538
meilech1 <c.meile@mail.utoronto.ca>
2023-06-10 10:16:54 -0400

    Updated user validation if block & comments associated as I caught a potential issue with decimals values.

--- Assignment2_Meile_Christopher.R
+++ Assignment2_Meile_Christopher.R
@@ -1,6 +1,9 @@
 # Assignment 2: Narcissistic Numbers
 
 #* INFORMATION ABOUT WORKING DIRECTORY / GIT INFO GOES HERE. TBD
+#* ASSUMPTION OVERVIEW.. will be touched upon later
+#* (1) 0 is not a positive number
+#* (2) narcissic numbers must be 3-digit long positive integers. 
 
 #* First, we will prompt the user to enter a three digit positive number. We
 #* assign this to the variable 'user_value'. The prompt gives an indication
@@ -27,20 +30,30 @@ if(!is.na(as.numeric(user_value))){
   
   user_number <- as.numeric(user_value)
   
-  #* The nested IF statement below checks two criteria. First, it checks that
-  #* the converted 'user_number' is greater than 0. This is our criteria
-  #* for the input being a positive number. Assumption: 0 is not a positive
-  #* number. Second, the IF statement checks if the number of characters in
+  #* The nested IF statement below checks three criteria: 
+  #* (1) First, it checks that the converted 'user_number' is greater than 0. 
+  #* This is our criteria for the input being a positive number. Assumption: 0 
+  #* is not a positive number. 
+  #* (2) Second, the IF statement checks if the number of characters in
   #* the number is equal to three. This is done using the nchar() function.
-  #* This is our three digit criteria. If these criteria are not met, the else 
-  #* block prints an error to the user with a prompt describing the error 
-  #* and what the user's input was. Script then terminates. 
+  #* This is our three digit criteria. 
+  #* (3) The third condition checks a potential risk with the 2 conditions above.
+  #* If the user inputs number "1.3" both the conditions above will return TRUE. 
+  #* However, we will make the assumption that narcissistic numbers can only
+  #* be integers. As such, the third condition checks if the 'user_number' is
+  #* an integer by using the is.intger() function which returns TRUE or FALSE.
+  #* If any of these 3 criteria are not met, the else  block prints an error to 
+  #* the user with a prompt describing the error and what the user's input was. 
+  #* Script then terminates. 
+
   
-  if(user_number > 0 & nchar(user_number) == 3) {
-    #* TRUE if 'user_number' is positive (assumption, 0 is not positive) AND if 
-    #* 'user_number' has 3 digits.
+  if(user_number > 0 & nchar(user_number) == 3 & is.integer(user_number)) {
+    #* TRUE if 'user_number' is positive AND if 'user_number' has 3 digits AND 
+    #* if 'user_number' is an integer. This marks the end of our 
+    #* "User Input Validation" block. 
     
     
+    print("GOOD")
     
   } else {
     #* FALSE if 'user_number" is not positive OR if the number of characters
@@ -48,7 +61,7 @@ if(!is.na(as.numeric(user_value))){
     #* that their input did not meet these criteria. Prints user's input to
     #* indicate what their input was. 
     
-    print(paste("Error! Your input number must be positive and 3 digits long.",
+    print(paste("Error! The input number must be a positive 3 digit long integer.",
     user_number, "does not meet these criteria. Quitting Script..."))
   }
   
@@ -57,7 +70,6 @@ if(!is.na(as.numeric(user_value))){
   #* Reminding the user of the input parameters using a print function and 
   #* showing them what they entered.
   
-  print(paste("Error! Please enter a positive three digit number.", user_value,
+  print(paste("Error! Please enter a positive three digit integer", user_value,
   "is not a valid entry. Qutting Script..."))
 }
-
