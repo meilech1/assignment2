5d051da7e26e910a2d10d5be2acde666df4ee5e9
meilech1 <c.meile@mail.utoronto.ca>
2023-06-10 09:10:40 -0400

    First commit. Added header and function to prompt user input.

---
+++ Assignment2_Meile_Christopher.R
@@ -0,0 +1,12 @@
+# Assignment 2: Narcissistic Numbers
+
+#* INFORMATION ABOUT WORKING DIRECTORY / GIT INFO GOES HERE. TBD
+
+#* First, we will prompt the user to enter a three digit positive number. We
+#* assign this to the variable 'user_value'. The prompt gives an indication
+#* to the user of what kind of value we expect, but we will validate the input
+#* later. For now, 'user_value' will be of type 'character' and can be anything.
+
+user_value <- readline(prompt = "Please enter a three digit positive number: ")
+
+
